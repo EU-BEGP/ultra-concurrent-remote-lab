62c38f2e4c715e175a10972b11686a55abe3b2bd
bpedrazaa <bpedrazaa@gmail.com>
2024-10-17 16:05:13 -0400

    [BACKEND] Cast account verification code

--- ucl_api/app/users/urls.py
+++ ucl_api/app/users/urls.py
@@ -4,17 +4,17 @@ from users import views
 app_name = "users"
 
 urlpatterns = [
-    path("signup/", views.CreateUserView.as_view(), name="create"),
-    path("token/", views.CreateTokenView.as_view(), name="token"),
-    path("me/", views.ManageUserView.as_view(), name="me"),
     path(
         "activate-account/",
         views.ActivateUserAccount.as_view(),
         name="activate-account",
     ),
+    path("me/", views.ManageUserView.as_view(), name="me"),
     path(
         "request-code/",
         views.RequestVerificationCode.as_view(),
         name="request-code",
     ),
+    path("signup/", views.CreateUserView.as_view(), name="create"),
+    path("token/", views.CreateTokenView.as_view(), name="token"),
 ]
